1b219bf64186dba03c31bc40f61b39552d17e19c
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-05 22:30:02 +0300

    js 2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+const userName = 'John';
+const greeting = "I'm John";
+const _profession = 'Developer';
+const $hobby = 'Walking';
+const favouriteDessert = 'cake';
+let pet_ = 'dog';
+let superNumber;
+superNumber = 17;
