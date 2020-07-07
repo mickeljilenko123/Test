7fa76c064c0740a5bcd5ad7fac7c29fd20eb0d28
mickeljilenko123 <mickeljilenko@gmail.com>
2020-07-07 12:08:37 +0300

    js 4.1

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-let goodNumber = 17;
-let anotherNumber = goodNumber;
-anotherNumber = 77;
-console.log(goodNumber);
-console.log(anotherNumber);
-let customer = {
-    name: 'John',
-    age: 17
-};
-const anotherCustomer = customer;
-anotherCustomer.age = 20;
-console.log(customer.age);
-console.log(anotherCustomer.age);
+let a = 4;
+let b = 2;
+console.log('a: ', a);
+console.log('b: ', [b]);
+console.log('a - b = ', [a - b]);
+console.log('a + b = ', [a + b]);
+console.log('a / b = ', [a / b]);
